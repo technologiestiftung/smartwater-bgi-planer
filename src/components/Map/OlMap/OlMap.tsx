d0ef262530f4cad4d3d0c951063668f8a18a86a7
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-10-15 18:04:25 +0200

    feat: remove unused code

--- src/components/Map/OlMap/OlMap.tsx
+++ src/components/Map/OlMap/OlMap.tsx
@@ -3,7 +3,6 @@
 import { useMapStore } from "@/store/map";
 import Map from "ol/Map";
 import View from "ol/View";
-import { transform } from "ol/proj";
 import React, { FC, useEffect, useRef } from "react";
 import "../../../../node_modules/ol/ol.css";
 
@@ -21,35 +20,12 @@ const OlMap: FC<OlMapProps> = ({ children }) => {
 		if (!config) return;
 
 		const mapViewConfig = config.portalConfig.map.mapView;
-
 		const projection = mapViewConfig.epsg;
-		let center = mapViewConfig.startCenter;
+		const center = mapViewConfig.startCenter;
 
-		// Check if coordinates are in WGS84 format (longitude/latitude)
-		// and transform them to the target projection if needed
-		if (
-			center.length === 2 &&
-			Math.abs(center[0]) <= 180 &&
-			Math.abs(center[1]) <= 90
-		) {
-			center = transform(center, "EPSG:4326", projection);
-		}
-
-		// If coordinates are not in WGS84 format, assume they are already
-		// in the target projection format and use them directly
 		const resolutions = mapViewConfig.options
 			.sort((a, b) => a.zoomLevel - b.zoomLevel)
 			.map((option) => option.resolution);
-
-		const startZoomLevel = Math.max(
-			0,
-			Math.min(mapViewConfig.startZoomLevel, resolutions.length - 1),
-		);
-		if (startZoomLevel !== mapViewConfig.startZoomLevel) {
-			console.warn(
-				`[OlMap] Start zoom level ${mapViewConfig.startZoomLevel} is out of range. Using ${startZoomLevel} instead.`,
-			);
-		}
 
 		if (!mapId.current) {
 			console.error("[OlMap] mapId.current is not defined");
@@ -61,7 +37,7 @@ const OlMap: FC<OlMapProps> = ({ children }) => {
 				target: mapId.current,
 				view: new View({
 					center: center,
-					zoom: startZoomLevel,
+					zoom: mapViewConfig.startZoomLevel,
 					projection: projection,
 					smoothResolutionConstraint: false,
 					constrainResolution: true,
