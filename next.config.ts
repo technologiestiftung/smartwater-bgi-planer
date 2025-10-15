018e1967a42018551173b48193aed40e84a57375
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-10-15 10:45:53 +0200

    feat: add image settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "gdi.berlin.de",
+			},
+			{
+				protocol: "https",
+				hostname: "smartwater-bgi-planer.netlify.app/",
+			},
+			{
+				protocol: "https",
+				hostname: "*smartwater-bgi-planer.netlify.app/",
+			},
+		],
+	},
+	devIndicators: false,
 };
 
 export default nextConfig;
