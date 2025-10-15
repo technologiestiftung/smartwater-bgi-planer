7594673f8b86773fd10fb90e3a9182fa12eb3299
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-10-15 14:06:48 +0200

    feat: add variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -19,12 +19,15 @@ const buttonVariants = cva(
 				ghost:
 					"hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
 				link: "text-primary underline-offset-4 hover:underline",
+				"map-icon":
+					"rounded-full shadow-md bg-background hover:bg-accent [&_svg]:size-6",
 			},
 			size: {
 				default: "h-9 px-4 py-2 has-[>svg]:px-3",
 				sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
 				lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
 				icon: "size-9",
+				"icon-only": "h-12 w-12",
 				"icon-sm": "size-8",
 				"icon-lg": "size-10",
 			},
