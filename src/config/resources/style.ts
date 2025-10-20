e812b2d2f4d67bc7c29e9983e5a86ace1b7c7140
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-10-20 17:46:30 +0200

    feat: add layer and module configs

--- src/config/resources/style.ts
+++ src/config/resources/style.ts
@@ -52,12 +52,120 @@ const styleList = [
 		],
 	},
 	{
+		styleId: "floodingHotspots",
+		rules: [
+			{
+				style: {
+					polygonStrokeWidth: 2,
+					polygonStrokeColor: [0, 119, 190, 1],
+					polygonFillColor: [0, 119, 190, 0.5],
+				},
+			},
+		],
+	},
+	{
+		styleId: "temperatureHotspots",
+		rules: [
+			{
+				style: {
+					polygonStrokeWidth: 2,
+					polygonStrokeColor: [255, 87, 34, 1],
+					polygonFillColor: [255, 87, 34, 0.5],
+				},
+			},
+		],
+	},
+	{
+		styleId: "impermeabilityHotspots",
+		rules: [
+			{
+				style: {
+					polygonStrokeWidth: 2,
+					polygonStrokeColor: [121, 85, 72, 1],
+					polygonFillColor: [121, 85, 72, 0.5],
+				},
+			},
+		],
+	},
+	{
+		styleId: "waterbalanceHotspots",
+		rules: [
+			{
+				style: {
+					polygonStrokeWidth: 2,
+					polygonStrokeColor: [3, 169, 244, 1],
+					polygonFillColor: [3, 169, 244, 0.5],
+				},
+			},
+		],
+	},
+	{
+		styleId: "groundwaterHotspots",
+		rules: [
+			{
+				style: {
+					polygonStrokeWidth: 2,
+					polygonStrokeColor: [0, 150, 136, 1],
+					polygonFillColor: [0, 150, 136, 0.5],
+				},
+			},
+		],
+	},
+	{
+		styleId: "feasibilityLayers",
+		rules: [
+			{
+				style: {
+					polygonStrokeWidth: 2,
+					polygonStrokeColor: [156, 39, 176, 1],
+					polygonFillColor: [156, 39, 176, 0.4],
+				},
+			},
+		],
+	},
+	{
+		styleId: "measuresLayers",
+		rules: [
+			{
+				style: {
+					polygonStrokeWidth: 2,
+					polygonStrokeColor: [76, 175, 80, 1],
+					polygonFillColor: [76, 175, 80, 0.5],
+				},
+			},
+		],
+	},
+	{
 		styleId: "module1Notes",
 		rules: [
 			{
 				style: {
 					pointFillColor: [255, 165, 0, 0.8],
 					pointStrokeColor: [255, 140, 0, 1],
+					pointStrokeWidth: 2,
+				},
+			},
+		],
+	},
+	{
+		styleId: "module2Notes",
+		rules: [
+			{
+				style: {
+					pointFillColor: [156, 39, 176, 0.8],
+					pointStrokeColor: [123, 31, 162, 1],
+					pointStrokeWidth: 2,
+				},
+			},
+		],
+	},
+	{
+		styleId: "module3Notes",
+		rules: [
+			{
+				style: {
+					pointFillColor: [76, 175, 80, 0.8],
+					pointStrokeColor: [56, 142, 60, 1],
 					pointStrokeWidth: 2,
 				},
 			},
