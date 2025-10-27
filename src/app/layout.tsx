52b030d8d2e0d18254c77a7846c4e3e221a9fb4e
Jonathan Haudenschild <jonathan.haudenschild@gmail.com>
2025-10-27 12:51:18 +0100

    feat: add fonts and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,20 @@ import MapInitializer from "@/components/Map/MapInitializer/MapInitializer";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./css/globals.css";
+import Background from "@/images/background.svg";
 
 const arthouseOwned = localFont({
 	src: [
+		{
+			path: "../../public/fonts/ArthouseOwnedLight.ttf",
+			weight: "300",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/ArthouseOwnedRegular.ttf",
+			weight: "400",
+			style: "normal",
+		},
 		{
 			path: "../../public/fonts/ArthouseOwnedMedium.ttf",
 			weight: "500",
@@ -37,14 +48,24 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
+	modal,
 }: Readonly<{
 	children: React.ReactNode;
+	modal: React.ReactNode;
 }>) {
 	return (
 		<html lang="de">
 			<body className={arthouseOwned.variable}>
-				<MapInitializer />
-				{children}
+				<div className="relative h-full w-full">
+					<div className="absolute h-full w-full">
+						<MapInitializer />
+						{children}
+						{modal}
+					</div>
+					<div className="bg-primary absolute -z-99 flex h-full w-full items-center justify-center overflow-hidden">
+						<Background className="min-h-full min-w-full flex-shrink-0" />
+					</div>
+				</div>
 			</body>
 		</html>
 	);
