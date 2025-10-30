f603c535ec41b10d93c550e332e835e147735305
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-10-30 13:12:34 +0100

    feat: add draw buttons

--- src/app/[id]/layout.tsx
+++ src/app/[id]/layout.tsx
@@ -1,5 +1,13 @@
+import BlockAreaSelector from "@/components/BlockAreaSelector/BlockAreaSelector";
+import {
+	DrawButton,
+	DrawMeasureButton,
+	DrawNoteButton,
+	DrawProjectBoundaryButton,
+} from "@/components/DrawControls";
 import Map from "@/components/Map/Map";
 import { MenuToggleButton } from "@/components/MenuToggleButton/MenuToggleButton";
+import UploadProjectBoundaryButton from "@/components/UploadProjectBoundaryButton/UploadProjectBoundaryButton";
 
 interface ProjectLayoutProps {
 	children: React.ReactNode;
@@ -16,6 +24,14 @@ export default async function ProjectLayout({
 	return (
 		<div className="relative h-full w-full">
 			<Map />
+			<div className="absolute right-40 bottom-4 z-1000 flex gap-2">
+				<DrawMeasureButton layerId="measures_seepage" />
+				<DrawButton />
+				<BlockAreaSelector />
+				<DrawProjectBoundaryButton />
+				<DrawNoteButton layerId="module1_notes" />
+				<UploadProjectBoundaryButton />
+			</div>
 			<MenuToggleButton projectId={id} />
 			{children}
 		</div>
