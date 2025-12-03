f4941b7eb9faffc4f1d39b8246815b22e5c0d88f
Luise Brandenburger <luise.brandenburger@ts.berlin>
2025-12-03 10:22:37 +0100

    refactor: remove useless code

--- src/lib/helpers/file.ts
+++ src/lib/helpers/file.ts
@@ -1,15 +1,5 @@
-/**
- * Generates a random layer ID based on the given idRoot.
- * @param {idRoot} The root of the layer ID
- * @returns A random layer ID
- */
 export const generateLayerId = (idRoot = "uploaded") =>
 	`${idRoot}_${Date.now()}_${Math.random().toString(36).slice(2, 9)}`;
 
-/**
- * Returns the file name from a full file name.
- * @param {fullName} The full file name
- * @returns The file name
- */
 export const getFileName = (fullName: string) =>
 	fullName.replace(/\.[^/.]+$/, "");
