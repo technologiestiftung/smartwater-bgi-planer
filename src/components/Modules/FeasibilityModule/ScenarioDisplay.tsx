1b0854c661394dcb19e58f28732071de6ab88a5b
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-01-19 12:31:41 +0100

    feat: update scenario display and icons

--- src/components/Modules/FeasibilityModule/ScenarioDisplay.tsx
+++ src/components/Modules/FeasibilityModule/ScenarioDisplay.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useLayersStore } from "@/store";
+import { useMapStore } from "@/store/map";
+import {
+	WaveSineIcon,
+	WaveSquareIcon,
+	WaveTriangleIcon,
+} from "@phosphor-icons/react";
 import { FC, useEffect, useState } from "react";
 
 interface ScenarioDisplayProps {}
@@ -10,25 +17,50 @@ const SCENARIOS = [
 		id: 1,
 		label: "einfache Umsetzung",
 		configId: "2V1",
+		icon: <WaveSineIcon />,
 	},
 	{
 		id: 2,
 		label: "gängige Praxis",
 		configId: "2V1.1",
+		icon: <WaveTriangleIcon />,
 	},
 	{
 		id: 3,
 		label: "aufwendige Umsetzung",
 		configId: "2V1.2",
+		icon: <WaveSquareIcon />,
 	},
 ];
 
 const ScenarioDisplay: FC<ScenarioDisplayProps> = ({}) => {
-	const [selectedScenario, setSelectedScenario] = useState(1);
+	const [selectedScenario, setSelectedScenario] = useState("2V1");
+	const [currentZoom, setCurrentZoom] = useState(0);
+	const applyConfigLayers = useLayersStore((state) => state.applyConfigLayers);
+	const map = useMapStore((state) => state.map);
 
 	useEffect(() => {
-		console.log("[ScenarioDisplay] ::", selectedScenario);
-	}, [selectedScenario]);
+		if (!map) return;
+
+		const view = map.getView();
+		if (!view) return;
+
+		const updateZoom = () => {
+			const zoom = view.getZoom();
+			setCurrentZoom(Math.round(zoom || 0));
+		};
+
+		updateZoom();
+
+		view.on("change:resolution", updateZoom);
+		return () => {
+			view.un("change:resolution", updateZoom);
+		};
+	}, [map]);
+
+	useEffect(() => {
+		applyConfigLayers(selectedScenario, true);
+	}, [selectedScenario, applyConfigLayers]);
 
 	return (
 		<div className="ScenarioDisplay-root">
@@ -38,15 +70,23 @@ const ScenarioDisplay: FC<ScenarioDisplayProps> = ({}) => {
 				unterschiedlichem Umsetzungsaufwand anzuzeigen.
 			</div>
 
-			<div className="flex flex-wrap gap-2">
+			<div className="mb-2 wrap-break-word">
+				Bitte beachten Sie, dass Sie zum Darstellen der Karten ein Zoomlevel von
+				mindestens 6 haben. Ihr aktuelles Zoomlevel ist {currentZoom}.
+			</div>
+
+			<div className="mb-4 flex flex-wrap gap-2">
 				{SCENARIOS.map((scenario) => (
 					<Button
 						key={scenario.id}
-						variant={"outline"}
+						variant={
+							selectedScenario === scenario.configId ? "default" : "outline"
+						}
 						size="sm"
-						onClick={() => setSelectedScenario(scenario.id)}
+						onClick={() => setSelectedScenario(scenario.configId)}
 						className="text-sm"
 					>
+						{scenario.icon}
 						{scenario.label}
 					</Button>
 				))}
