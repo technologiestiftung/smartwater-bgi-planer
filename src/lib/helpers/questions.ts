ef7be596ae0dc4c2994abd463dbd687d88b00c8d
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-01-20 09:55:25 +0100

    feat: remove 2V1 question

--- src/lib/helpers/questions.ts
+++ src/lib/helpers/questions.ts
@@ -1,5 +1,5 @@
 export function checkForQuestion(string: string, shouldInclude = false) {
-	const questionRegex = /^(.*?)(_starter_question|_module_introduction)$/;
+	const questionRegex = /^(.*?)(_starter_question|_module_introduction|2V1)$/;
 	if (shouldInclude) {
 		return questionRegex.test(string);
 	}
