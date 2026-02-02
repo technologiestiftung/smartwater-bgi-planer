ee39832304fc1db4caf022e41fefbc853622c1b9
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-02-02 11:18:33 +0100

    refactor: clean up

--- src/components/Map/Controls/index.ts
+++
@@ -1,6 +0,0 @@
-import MapControls from "./MapControls";
-import GeolocationControl from "./MapNavigation/GeolocationControl";
-import MapNavigationContainer from "./MapNavigation/MapNavigationContainer";
-import ZoomControl from "./MapNavigation/ZoomControl";
-
-export { GeolocationControl, MapControls, MapNavigationContainer, ZoomControl };
