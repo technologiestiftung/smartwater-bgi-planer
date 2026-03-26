821d5af65817b1ad34373d752dad7ebc16ea2575
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-03-26 09:52:30 +0100

    fix: packages

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Slider as SliderPrimitive } from "radix-ui";
+import * as SliderPrimitive from "@radix-ui/react-slider";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
@@ -13,15 +13,17 @@ function Slider({
 	max = 100,
 	...props
 }: React.ComponentProps<typeof SliderPrimitive.Root>) {
-	const _values = React.useMemo(
-		() =>
-			Array.isArray(value)
-				? value
-				: Array.isArray(defaultValue)
-					? defaultValue
-					: [min, max],
-		[value, defaultValue, min, max],
-	);
+	const _values = React.useMemo(() => {
+		if (Array.isArray(value)) {
+			return value;
+		}
+
+		if (Array.isArray(defaultValue)) {
+			return defaultValue;
+		}
+
+		return [min, max];
+	}, [value, defaultValue, min, max]);
 
 	return (
 		<SliderPrimitive.Root
@@ -31,7 +33,7 @@ function Slider({
 			min={min}
 			max={max}
 			className={cn(
-				"relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col",
+				"relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col",
 				className,
 			)}
 			{...props}
