7e6a33dff08b815e6e605806c71bb08ca146f1df
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-06-10 10:41:48 +0200

    refactor: remove function

--- src/lib/helpers/measures/config.ts
+++ src/lib/helpers/measures/config.ts
@@ -1,12 +1,4 @@
-import type { MeasureConfig, MeasureGeometryType } from "@/types/measures";
+import type { MeasureConfig } from "@/types/measures";
 
 export const createMeasureConfigMap = (configs: MeasureConfig[]) =>
 	new Map(configs.map((config) => [config.id, config] as const));
-
-export const normalizeMeasureGeometryType = (
-	geometryType: string | undefined,
-): MeasureGeometryType => {
-	if (geometryType === "Point") return "Point";
-	if (geometryType === "Circle") return "Circle";
-	return "Polygon";
-};
