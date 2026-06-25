da56978ea37f88cb346caf7a94d8012c7ebdf0c8
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-06-25 10:53:06 +0200

    feat: add missing key

--- src/lib/simulation/calculations/measureCalculations.ts
+++ src/lib/simulation/calculations/measureCalculations.ts
@@ -5,6 +5,7 @@ import areaCalc from "./areaCalculations";
 const connectedAreaFieldByMeasure = {
 	to_swale: "to_swale",
 	to_swale_trench: "to_swale_trench",
+	to_surf_infil: "to_surf_infil",
 	to_trench: "to_trench",
 	to_cistern: "to_cistern",
 	to_tree_pit: "to_tree_pit",
