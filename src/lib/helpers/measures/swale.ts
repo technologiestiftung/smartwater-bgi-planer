60bb9d2bf96653173c69e1dd93e434cec18f3077
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-06-25 17:56:31 +0200

    fix: formatter

--- src/lib/helpers/measures/swale.ts
+++ src/lib/helpers/measures/swale.ts
@@ -1,4 +1,11 @@
-const SWALE_LAYER_CONFIG_IDS = new Set(["3V1", "3V2", "3V3", "3V4", "3V5", "3V6"]);
+const SWALE_LAYER_CONFIG_IDS = new Set([
+	"3V1",
+	"3V2",
+	"3V3",
+	"3V4",
+	"3V5",
+	"3V6",
+]);
 
 export const isSwaleLayerConfigId = (
 	layerConfigId: string | null | undefined,
