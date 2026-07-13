6c7bc52eb4c0a9c1cf45113668fe280684f903c7
Jakob Sawal <jakob.sawal@ts.berlin>
2026-07-13 14:32:02 +0200

    fix: remove texts from ClimateSimulation view

--- src/components/Simulations/ClimateSimulation.tsx
+++ src/components/Simulations/ClimateSimulation.tsx
@@ -28,11 +28,7 @@ export function ClimateSimulation({
 		"measurePlanning",
 		climateSimulation,
 	) as ModuleStepConfig;
-	const {
-		title,
-		info: { description: infoDescription } = {},
-		climateSimulation: { description } = {},
-	} = getModuleInfo || {};
+	const { title } = getModuleInfo || {};
 
 	return (
 		<div className="flex h-full w-full flex-col">
@@ -40,8 +36,6 @@ export function ClimateSimulation({
 				<h3 className="text-primary">
 					Simulationsergebnisse Stadtklima - {title}
 				</h3>
-				<p className="font-bold">{infoDescription}</p>
-				{description && <p>{description}</p>}
 			</div>
 			<div className="border-muted bg-secondary flex shrink-0 border-t px-4">
 				<Button
