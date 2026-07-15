547d5e025a43766c745edf51f1c4356c9c553cf3
Jakob Sawal <jakob.sawal@ts.berlin>
2026-07-15 09:58:26 +0200

    fix: remove comment

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,6 +1,5 @@
 import { ProjectModal } from "@/components/ProjectModal/ProjectModal";
 
 export default function NewProjectPage() {
-	// comment to be removed
 	return <ProjectModal mode="new" />;
 }
