940802ea819ad2ad3a6844506c809c29ca244121
Jakob Sawal <jakob.sawal@ts.berlin>
2026-07-15 09:54:37 +0200

    fix: testing redeploy

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,6 @@
 import { ProjectModal } from "@/components/ProjectModal/ProjectModal";
 
 export default function NewProjectPage() {
+	// comment to be removed
 	return <ProjectModal mode="new" />;
 }
